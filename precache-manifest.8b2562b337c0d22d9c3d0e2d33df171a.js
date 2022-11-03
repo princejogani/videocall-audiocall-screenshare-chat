5388594bc1ccccf6eebeb466121e771e48746a89
princejogani <joganiprince1@gmail.com>
2022-11-03 12:21:22 +0530

    Updates

---
+++ precache-manifest.8b2562b337c0d22d9c3d0e2d33df171a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177d31a0bcdc2986995c239e1cdaa39c",
+    "url": "/videocall-audiocall-screenshare-chat/index.html"
+  },
+  {
+    "revision": "9aaceb2bdded717030b9",
+    "url": "/videocall-audiocall-screenshare-chat/static/css/main.dfa5c60d.chunk.css"
+  },
+  {
+    "revision": "38716537270f76ab5d5d",
+    "url": "/videocall-audiocall-screenshare-chat/static/js/2.c07367c9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/videocall-audiocall-screenshare-chat/static/js/2.c07367c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aaceb2bdded717030b9",
+    "url": "/videocall-audiocall-screenshare-chat/static/js/main.47a333ba.chunk.js"
+  },
+  {
+    "revision": "efb4eabae34f103066f3",
+    "url": "/videocall-audiocall-screenshare-chat/static/js/runtime-main.6ba6e4ea.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/videocall-audiocall-screenshare-chat/precache-manifest.8b2562b337c0d22d9c3d0e2d33df171a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/videocall-audiocall-screenshare-chat/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
